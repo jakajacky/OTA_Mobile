d8408c0cd65deca62a4e4aa40324493ed946dbf1
jakajacky <jack_zhangx@126.com>
2020-10-19 12:34:42 +0800

    [add]暂时不需要生成二维码

--- server/upload.js
+++ server/upload.js
@@ -3,10 +3,12 @@
  */
 var path = require('path');
 var fs = require('fs');//文件模块
+var qr = require('qr-image');
+var plist = require('plist');
 
 var port = process.env.PORT || '8100';//默认端口8100
 
-var uploadHost = `http://localhost:${port}/uploads/`;//图片可访问地址
+var uploadHost = `http://172.20.110.150:${port}/uploads/`;//图片可访问地址
 
 async function uploadController(ctx) {
     if (ctx.path === '/upfile') {
@@ -30,6 +32,23 @@ async function uploadController(ctx) {
             //});
             // 以字符串 形式输出上传文件地址
             ctx.body = `${uploadHost}${nextPath.slice(nextPath.lastIndexOf('/') + 1)}`
+
+            // 生成plist
+            var data = fs.readFileSync(`${__dirname}/manifest_tmp.plist`, 'utf-8')
+            var obj = plist.parse(data, 'utf-8')
+            obj.items[0].assets[0].url = `${uploadHost}${nextPath.slice(nextPath.lastIndexOf('/') + 1)}`
+            obj = plist.build(obj)
+            fs.writeFile(`${__dirname}/../static/uploads/manifest.plist`, obj, err => {
+                if (err) {
+                    // 错误处理
+                }
+            })
+            // 直接扫码安装
+            //生成二维码
+            var qr_svg = qr.image(`itms-services://?action=download-manifest&url=${uploadHost}manifest.plist`, { type: 'png' });
+            //将二维码输出到文件流，并生成png文件
+            qr_svg.pipe(require('fs').createWriteStream(`${__dirname}/../static/uploads/qr.png`));
+
 
         }else {
             ctx.body = getRenderData({
